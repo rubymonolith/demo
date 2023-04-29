d00dd6eb8809737d076d2b1db237f1604eac1dcc
Brad Gessler <bradgessler@gmail.com>
2023-04-29 10:51:10 -0700

    Add Item to Batch

--- app/controllers/blogs/batch/posts_controller.rb
+++ app/controllers/blogs/batch/posts_controller.rb
@@ -1,27 +1,43 @@
 class Blogs::Batch::PostsController < ApplicationController
-  resources :posts, through: :blogs, from: :current_user
+  before_action :assign_batch
+  assign :posts, through: :blogs, from: :current_user
+  # TODO: How can I compose `assign :posts, ... with `resources`?
 
   include Batchable
 
-  class Batch
-    include ActiveModel::API
-    attr_accessor :ids, :scope
+  class ItemForm < ApplicationForm
   end
 
   class Index < ApplicationView
-    attr_accessor :posts, :blog, :current_user
+    attr_accessor :posts, :blog, :current_user, :batch
 
     def title = "#{@blog.title} Posts"
     def subtitle = "Select posts"
 
     def template(&)
-      form url: blog_batch_posts_path(@blog), method: :post do
-        list(@blog.posts) do |post|
-          show(post, :title)
+      render ApplicationForm.new(@batch, url: url_for) do |form|
+        render TableComponent.new(items: @batch) do |table|
+          table.column do |column|
+            column.item do |selection|
+              input(type: "checkbox", value: selection.selected, name: "batch[items][#{selection.id}][selected]")
+              # render ItemForm.new selection
+            end
+          end
+          table.column("Title")         { show(_1.item, :title) }
+          table.column("Author")        { show(_1.item.user, :name) }
+          table.column("Status")        { "Not Published" }
+          table.column("Publish Date")  { _1.item.publish_at&.to_formatted_s(:long) }
         end
+
         nav do
-          button(value: :delete, name: "batch[action]") { "Delete" }
-          button(value: :publish, name: "batch[action]") { "Publish" }
+          ul do
+            li do
+              form.button_field(:action, value: :delete) { "Delete" }
+            end
+            li do
+              form.button_field(:action, value: :publish) { "Publish" }
+            end
+          end
         end
       end
     end
--- app/controllers/concerns/batchable.rb
+++ app/controllers/concerns/batchable.rb
@@ -1,14 +1,56 @@
 module Batchable
   extend ActiveSupport::Concern
 
+  class Batch
+    include Enumerable
+
+    include ActiveModel::API
+    attr_accessor :items, :scope, :action
+
+    class Item
+      include ActiveModel::API
+      attr_accessor :selected, :item
+
+      delegate :id, to: :item
+    end
+
+    def selection
+      @scope.where(id: ids)
+    end
+
+    def each
+      @scope.each do |item|
+        yield Item.new(item: item, selected: false)
+      end
+    end
+
+    def self.action_param_key
+      "action"
+    end
+
+    private
+
+    def selected_ids
+      raise "hell"
+    end
+  end
+
   protected
+
+  def assign_batch
+    @batch = Batch.new(scope: @blog.posts, **permitted_batch_params)
+  end
+
+  def permitted_batch_params
+    params.fetch(Batch.model_name.param_key, {}).permit(:action, items: [:selected])
+  end
 
   def method_for_action(action_name)
     routable_batch_action? ? batch_action : super
   end
 
   def batch_action
-    params.dig("batch", "action")
+    params.dig Batch.model_name.param_key, Batch.action_param_key
   end
 
   def routable_batch_action?
